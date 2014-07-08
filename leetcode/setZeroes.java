dbe7e7e0b2692212357e85350550854daa5a6daf
upbeat21 <yangsong0609@gmail.com>
2014-07-07 17:10:30 -0700

    Create setZeroes.java

---
+++ leetcode/setZeroes.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+		int col = matrix[0].length;
+		boolean firstrowzero = false;
+		boolean firstcolzero = false;
+		for(int i=0;i<row;i++) {
+			if(matrix[i][0] == 0) {
+				firstcolzero = true;
+				break;
+			}
+		}
+		for(int i=0;i<col;i++) {
+			if(matrix[0][i] == 0) {
+				firstrowzero = true;
+				break;
+			}
+		}
+		for(int i=1;i<row;i++) {
+			for(int j=1;j<col;j++) {
+				if(matrix[i][j] == 0) {
+					matrix[0][j] = 0;
+					matrix[i][0] = 0;
+				}
+			}
+		}
+		for(int i=1;i<row;i++) {
+			for(int j=1;j<col;j++) {
+				if(matrix[0][j] == 0 || matrix[i][0] == 0) matrix[i][j] = 0;
+			}
+		}
+		if(firstcolzero) {
+			for(int i=0;i<row;i++) matrix[i][0] = 0;
+		}
+		if(firstrowzero) {
+			for(int i=0;i<col;i++) matrix[0][i] = 0;
+		}
+    }
+}
