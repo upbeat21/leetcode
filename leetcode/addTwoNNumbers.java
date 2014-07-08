7a8f568b12c287fc17208f05cab907136ea3ff81
upbeat21 <yangsong0609@gmail.com>
2014-07-08 11:43:57 -0700

    Update addTwoNNumbers.java

--- leetcode/addTwoNNumbers.java
+++ leetcode/addTwoNNumbers.java
@@ -11,35 +11,32 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l3 = new ListNode(-1);
-        ListNode curr = l3;
-        int k = 0;
-        while(l1 != null && l2 != null) {
-            int a = l1.val;
-            int b = l2.val;
-            curr.next = new ListNode((a + b + k) % 10);
-            k = (a + b + k) / 10;
-            curr = curr.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        if(l1 == null) {
-            while(l2 != null) {
-                curr.next = new ListNode((l2.val + k) % 10);
-                k = (l2.val + k) / 10;
-                curr = curr.next;
-                l2 = l2.next;
-            }
-        }
-        if(l2 == null) {
-            while(l1 != null) {
-                curr.next = new ListNode((l1.val + k) % 10);
-                k = (l1.val + k) / 10;
-                curr = curr.next;
-                l1 = l1.next;
-            }
-        }
-        if(k != 0) curr.next = new ListNode(k);
-        return l3.next;
+        ListNode fake = new ListNode(-1);
+		ListNode curr = fake;
+		int t = 0;
+		while(l1 != null && l2 != null) {
+			int n = (l1.val + l2.val + t) % 10;
+			t = (l1.val + l2.val + t) / 10;
+			curr.next = new ListNode(n);
+			curr = curr.next;
+			l1 = l1.next;
+			l2 = l2.next;
+		}
+		while(l1 != null) {
+			int n = (l1.val + t) % 10;
+			t = (l1.val + t) / 10;
+			curr.next = new ListNode(n);
+			curr = curr.next;
+			l1 = l1.next;
+		}
+		while(l2 != null) {
+			int n = (l2.val + t) % 10;
+			t = (l2.val + t) / 10;
+			curr.next = new ListNode(n);
+			curr = curr.next;
+			l2 = l2.next;
+		}
+		if(t != 0) curr.next = new ListNode(t);
+		return fake.next;
     }
 }
