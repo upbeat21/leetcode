b377097ce4aba97150ed76e15cc0f4d67f74c82c
upbeat21 <yangsong0609@gmail.com>
2014-07-28 11:35:35 -0700

    Update longestConsecutive.java

--- leetcode/longestConsecutive.java
+++ leetcode/longestConsecutive.java
@@ -2,31 +2,23 @@ public class Solution {
     public int longestConsecutive(int[] num) {
         Set<Integer> set = new HashSet<Integer>();
 		for(int n : num) set.add(n);
-		int r = 0;
+		int result = 0;
 		for(int i=0;i<num.length;i++) {
-			int l = 1;
+			int sum = 1;
 			int j = 1;
-			while(true) {
-				int tmp = num[i] - j;
-				if(set.contains(tmp)) {
-					set.remove(tmp);
-					l++;
-					j++;
-				}
-				else break;
+			while(set.contains(num[i]-j)) {
+				set.remove(num[i]-j);
+				j++;
+				sum++;
 			}
 			j = 1;
-			while(true) {
-				int tmp = num[i] + j;
-				if(set.contains(tmp)) {
-					set.remove(tmp);
-					l++;
-					j++;
-				}
-				else break;
+			while(set.contains(num[i]+j)) {
+				set.remove(num[i]+j);
+				j++;
+				sum++;
 			}
-			if(r < l) r = l;
+			result = Math.max(result, sum);
 		}
-		return r;
+		return result;
     }
 }
