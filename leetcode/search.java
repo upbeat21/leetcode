778a3796fc438337e1b2eddc9730c28024b415e2
upbeat21 <yangsong0609@gmail.com>
2014-08-12 21:18:47 -0700

    Update search.java

--- leetcode/search.java
+++ leetcode/search.java
@@ -1,33 +1,26 @@
 public class Solution {
     public int search(int[] A, int target) {
         int l = 0;
-		int r = A.length-1;
-		while(l <= r) {
-			int m = (l + r) / 2;
-			if(A[m] >= A[0]) {
-				if(target >= A[0]) {
-					if(target == A[m]) return m;
-					else if(target > A[m]) {
-						l = m + 1;
-					} else {
-						r = m-1;
-					}
-				} else {
-					l = m + 1;
-				}
-			} else {
-				if(target >= A[0]) {
-					r = m - 1;
-				} else {
-					if(target == A[m]) return m;
-					else if(target > A[m]) {
-						l = m + 1;
-					} else {
-						r = m - 1;
-					}
-				}
-			}
-		}
-		return -1;
+        int r = A.length - 1;
+        while(l <= r) {
+            int m = (l + r) / 2;
+            int n = A[m];
+            if(n >= A[0]) {
+                if(target == n) return m;
+                else if(target > n) l = m + 1;
+                else {
+                    if(target >= A[0]) r = m - 1;
+                    else l = m + 1;
+                }
+            } else {
+                if(target == n) return m;
+                else if(target < n) r = m - 1;
+                else {
+                    if(target >= A[0]) r = m - 1;
+                    else l = m + 1;
+                }
+            }
+        }
+        return -1;
     }
 }
